d48ea16e460507a39aa32bed8e032076a6905779
Rob Nowell <robert.j.nowell@gmail.com>
2017-02-20 20:43:56 -0800

    change shell file structure

--- shells/hangman_wireframe4.sh
+++ shells/hangman_wireframe4.sh
@@ -1,6 +1,6 @@
 osascript 	-e 'tell application "terminal" to activate' \
 			-e 'tell application "System Events" to tell process "terminal" to keystroke "]" using {shift down, command down}' \
 			-e 'tell application "System Events" to tell process "terminal" to keystroke "c" using control down' \
-			-e 'tell application "System Events" to tell process "terminal" to keystroke "./wireframe 4.fdf"' \
+			-e 'tell application "System Events" to tell process "terminal" to keystroke "./wireframe wireframe_files/4.fdf"' \
 			-e 'tell application "System Events" to tell process "terminal" to key code 52' \
 			-e 'tell application "System Events" to tell process "terminal" to keystroke "[" using {shift down, command down}'
